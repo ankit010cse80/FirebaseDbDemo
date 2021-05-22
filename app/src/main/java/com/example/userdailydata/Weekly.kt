3edb01023eb01ede1ce1bd59c80aac5c0acedcec
ayansharma2 <ayansharma2341@gmail.com>
2021-05-22 16:39:51 +0530

    Bug Resolved

--- app/src/main/java/com/example/userdailydata/Weekly.kt
+++ app/src/main/java/com/example/userdailydata/Weekly.kt
@@ -29,8 +29,7 @@ import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.reflect.typeOf
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
